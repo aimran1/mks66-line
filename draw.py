dfefe028d1f9bad23cd63d345b11ffed05f98db3
aimran <aimran@stuycs.org>
2019-02-08 15:18:55 -0500

    Fixed horizontal line

--- draw.py
+++ draw.py
@@ -6,7 +6,7 @@ def draw_line( x0, y0, x1, y1, screen, color ):
     A = y1-y0
     B = (x1-x0) * -1
     #Octant 1
-    if (A > 0 and x1 - x0 >= A):
+    if (A >= 0 and x1 - x0 >= A):
         d = 2 * A + B
         while x <= x1:
             plot(screen,color,x,y)
@@ -25,5 +25,7 @@ def draw_line( x0, y0, x1, y1, screen, color ):
                 d += 2 * A
             y+=1
             d += 2*B
+    #Octant 3
+    #elif (A < 0 and 
             
         
