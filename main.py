716e519fbc0a8c0c892b0eae15238027274447cd
aimran <aimran@stuycs.org>
2019-02-08 15:17:37 -0500

    Octant 2 works

--- main.py
+++ main.py
@@ -3,7 +3,15 @@ from draw import *
 
 screen = new_screen()
 color = [ 0, 255, 0 ]
-
-draw_line(0,0,100,80,screen,color)
+#45 Degrees
+draw_line(0,0,500,500,screen,color)
+#Vertical
+draw_line(0,0,0,500,screen,color)
+#Horizontal
+draw_line(0,0,500,0,screen,color)
+#Octant 1
+draw_line(0,0,500,100,screen,color)
+#Octant 2
+draw_line(0,0,100,500,screen,color)
 display(screen)
 save_extension(screen, 'img.png')
